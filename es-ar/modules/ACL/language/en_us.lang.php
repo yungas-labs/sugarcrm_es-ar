1e3954fb6bd93cae876f6f61561b91466ef61cf3
sovietiko <sovietiko@lautaro-laptop.(none)>
2012-07-12 17:06:51 -0300

    Traducidos 4 modulos
    Traducidos los 4 modulos que tenia adjudicados hasta la fecha.

--- es-ar/modules/ACL/language/en_us.lang.php
+++ es-ar/modules/ACL/language/en_us.lang.php
@@ -36,21 +36,21 @@ if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
  ********************************************************************************/
 
 $mod_strings = array (
-'LBL_ALLOW_ALL'=>'All',
-'LBL_ALLOW_NONE'=>'None',
-'LBL_ALLOW_OWNER'=>'Owner',
-'LBL_ROLE'=>'Role',
-'LBL_NAME'=>'Name',
-'LBL_DESCRIPTION'=>'Description',
-'LIST_ROLES'=>'List Roles',
-'LBL_USERS_SUBPANEL_TITLE'=>'Users',
-'LIST_ROLES_BY_USER'=>'List Roles By User',
-'LBL_ROLES_SUBPANEL_TITLE'=>'User Roles',
-'LBL_SEARCH_FORM_TITLE'=>'Search',
-'LBL_NO_ACCESS'=>'You do not have access to this area. Contact your site administrator to obtain access.',
-'LBL_REDIRECT_TO_HOME'=>'Redirect to Home in',
-'LBL_SECONDS'=>'seconds',
-'LBL_ADDING'=>'Adding for ',
+'LBL_ALLOW_ALL'=>'Todo',
+'LBL_ALLOW_NONE'=>'Ninguno',
+'LBL_ALLOW_OWNER'=>'Dueño',
+'LBL_ROLE'=>'Rol',
+'LBL_NAME'=>'Nombre',
+'LBL_DESCRIPTION'=>'Descripción',
+'LIST_ROLES'=>'Listado de Roles',
+'LBL_USERS_SUBPANEL_TITLE'=>'Usuarios',
+'LIST_ROLES_BY_USER'=>'Listado de Roles por Usuario',
+'LBL_ROLES_SUBPANEL_TITLE'=>'Roles de los Usuarios',
+'LBL_SEARCH_FORM_TITLE'=>'Búsqueda',
+'LBL_NO_ACCESS'=>'No tenes acceso a esta área. Contacta con tu administrador del sitio para obtener acceso.',
+'LBL_REDIRECT_TO_HOME'=>'Redirecciónar a Inicio en',
+'LBL_SECONDS'=>'segundos',
+'LBL_ADDING'=>'Adhiriendo Para ',
 
 )
 ?>
